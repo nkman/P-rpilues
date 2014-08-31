39d5258ffba239d91a80cd759ca9183a1a93947a
Nairitya Khilari <nkmann2@gmail.com>
2014-09-01 02:43:58 +0530

    Add : cookie support
    cookies are saved in Redis-server.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -23,6 +23,26 @@ module.exports = {
 			else
 				return res.send(result, 200);
 		});
+	},
+
+	login: function(req, res){
+		var username = req.param('username');
+		var password = req.param('password');
+
+		if(!username || !password)
+			return res.send("Error", 400);
+
+		sails.log.info(username+" "+password);
+		sails.log.info(req.signedCookies['sails.sid']);
+		signinService.login(username, password, req.signedCookies, function(err, result){
+			if(err)
+				return res.send(err, 500);
+			else{
+				// res.cookie('user', user.cookies, { expires: new Date(Date.now() + 900000), httpOnly: true });
+				res.cookie('user', result.cookies);
+				return res.send(result, 200);
+			}
+		});
 	}
 };
 
--- api/services/signinService.js
+++ api/services/signinService.js
@@ -1,14 +1,21 @@
 var bcrypt = require('bcrypt');
+var redis = require("redis");
+var crypto = require('crypto');
 
-exports.signinUser = function(username, password, cb){
-	User.findOne({'username': username}).done(function(err, user){
+var client = redis.createClient();
+client.on("error", function (err) {
+	console.log("Error creating redis client : " + err);
+});
+
+exports.login = function(username, password, cookie, cb){
+	User.findOne({'username': username}, function(err, user){
 		if(err)
 			return cb(err, null);
 		if(!user || user.length == 0)
 			return cb(null, "No such user");
 
 		var hash = user.password;
-		bcrypt.compare(password,hash,function(err,result){
+		bcrypt.compare(password, hash, function(err,result){
 			if(err)
 				return cb(err, null);
 
@@ -17,7 +24,15 @@ exports.signinUser = function(username, password, cb){
 			else if(result){
 				delete user.createdAt;
 				delete user.updatedAt;
-				return cb(null, user);
+
+				client.set(user.id, cookie['sails.sid'], function(err, set){
+					if(err){
+						sails.log.error(err);
+						return cb({errorMessage:"Redis cannot set the token !!",errorCode:"REDIS_ERROR"},null);
+					}
+					user.cookies = cookie['sails.sid'];
+					return cb(null, user);
+				});
 			}
 		});
 	});
